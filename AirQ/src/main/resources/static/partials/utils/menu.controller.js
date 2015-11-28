7359b19b72ac40e775636242615a83cc471c9c59
Andrei Lescu <andrei.lescu@dialogdata.de>
2015-11-28 22:06:04 +0200

    Added select Menu field, now this field is activate by matching between path and href, added addition controller that maps the logic.

--- AirQ/src/main/resources/static/partials/utils/menu.controller.js
+++ AirQ/src/main/resources/static/partials/utils/menu.controller.js
@@ -1,11 +1,17 @@
 angular.module('airQ')
 .controller('menuController', function($scope, $location) {
 	$scope.getClass = function(path) {
-		if($location.path().substr(0, path.lenth) === path) {
-			console.log("HELLO" + "RETURN ACTIVE" + path);
-			return 'active';
+		var basePath = $location.path().substr(0, path.lenth);
+		var active = (basePath === path);	
+		if(active) {
+			return active;
 		} else {
 			return '';
 		}
-	}
+	};
+	
+	$scope.isActive = function (viewLocation) {
+	     var active = (viewLocation === $location.path());
+	     return active;
+	};
 });
